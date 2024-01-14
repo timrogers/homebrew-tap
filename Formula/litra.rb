09a32e573679c765925d729cc3e19c8c87a22df5
Tim Rogers <me@timrogers.co.uk>
2024-01-14 19:48:23 +0000

    Fix SHA256 for Litra

--- Formula/litra.rb
+++ Formula/litra.rb
@@ -2,7 +2,7 @@ class Litra < Formula
   desc "Control your Logitech Litra light from the command line"
   homepage "https://github.com/timrogers/litra-rs"
   url "https://github.com/timrogers/litra-rs/releases/download/v0.1.0/litra_v0.1.0_darwin-amd64"
-  sha256 "9e237022500adeb0a981219dc8666b2939c413db"
+  sha256 "e71610c5d1a62b967e09cd331c93fb8897843614315b2ab5a97674470abb4c76"
   version "0.1.0"
 
   def install
