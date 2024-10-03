a0830bad48e62cd43b6a1648efe00f28b3be52a2
Tim Rogers <timrogers@github.com>
2024-10-03 09:46:45 -0400

    litra 1.6.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/litra.rb
+++ Formula/litra.rb
@@ -1,9 +1,9 @@
 class Litra < Formula
   desc "Control your Logitech Litra light from the command line"
   homepage "https://github.com/timrogers/litra-rs"
-  url "https://github.com/timrogers/litra-rs/releases/download/v1.5.0/litra_v1.5.0_darwin-universal"
-  sha256 "0f9bbfa96d621b4d819ba69d03a3b3490e4608a0e185b83c29cc744db99be996"
-  version "1.5.0"
+  url "https://github.com/timrogers/litra-rs/releases/download/v1.6.0/litra_v1.6.0_darwin-universal"
+  sha256 "f8c753bdac880bbd5ad433b072b369c0ada44913f3e2413fab496e9b9fffd65f"
+  version "1.6.0"
 
   def install
     bin.install "litra_v#{version}_darwin-universal" => "litra"
