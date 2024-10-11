ad5867cc9a63d46834560f7ad848ca8492d0725e
Tim Rogers <timrogers@github.com>
2024-10-11 12:23:06 +0100

    Add Linux support for litra-autotoggle formula

--- Formula/litra-autotoggle.rb
+++ Formula/litra-autotoggle.rb
@@ -1,12 +1,27 @@
 class LitraAutotoggle < Formula
   desc 'Automatically turn your Logitech Litra device on when your webcam turns on, and off when your webcam turns off (macOS and Linux only)'
   homepage 'https://github.com/timrogers/litra-autotoggle'
-  url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.2.0/litra-autotoggle_v0.2.0_darwin-universal'
-  sha256 '7fd08516257d9baaf025eb199f4114abd3663904f41d7ca14efce88436708a37'
   version '0.2.0'
 
+  # Set the URL and SHA256 based on the platform
+  if OS.mac?
+    url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.2.0/litra-autotoggle_v0.2.0_darwin-universal'
+    sha256 '7fd08516257d9baaf025eb199f4114abd3663904f41d7ca14efce88436708a37'
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      odie "Linux devices with ARM processors are not supported"
+    else
+      url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.2.0/litra-autotoggle_v0.2.0_linux-amd64'
+      sha256 '244ad4cf5b40be246a1be9e75274a441592562b0be9b2715c47688877ff1e561'
+    end
+  end
+
   def install
-    bin.install "litra-autotoggle_v#{version}_darwin-universal" => 'litra-autotoggle'
+    if OS.mac?
+      bin.install "litra-autotoggle_v#{version}_darwin-universal" => 'litra-autotoggle'
+    else
+      bin.install "litra-autotoggle_v#{version}_linux-amd64" => 'litra-autotoggle'
+    end
   end
 
   service do
