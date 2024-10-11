2f557f3554fe4522f81c23eaabdf74e18a8a8417
Tim Rogers <me@timrogers.co.uk>
2024-10-11 16:49:16 +0100

    Update litra and litra-autotoggle to latest versions, with ARM Linux support

--- Formula/litra-autotoggle.rb
+++ Formula/litra-autotoggle.rb
@@ -5,27 +5,30 @@ class LitraAutotoggle < Formula
 
   # Set the URL and SHA256 based on the platform
   if OS.mac?
-    url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.2.0/litra-autotoggle_v0.2.0_darwin-universal'
-    sha256 '7fd08516257d9baaf025eb199f4114abd3663904f41d7ca14efce88436708a37'
+    url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.3.0/litra-autotoggle_v0.3.0_darwin-universal'
+    sha256 '8ca0405f5567bd9eaae9fa16b814e9af1e4b1b6bcbd4bbe01f7e8af14581e360'
   elsif OS.linux?
     if Hardware::CPU.arm?
-      odie "Linux devices with ARM processors are not supported"
+      url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.3.0/litra-autotoggle_v0.3.0_linux-aarch64'
+      sha256 '6b94bf961d291e9718f3fbd996396b64a92555e0b6a0eab554bffe0518f08fb4'
     else
-      url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.2.0/litra-autotoggle_v0.2.0_linux-amd64'
-      sha256 '244ad4cf5b40be246a1be9e75274a441592562b0be9b2715c47688877ff1e561'
+      url 'https://github.com/timrogers/litra-autotoggle/releases/download/v0.3.0/litra-autotoggle_v0.3.0_linux-amd64'
+      sha256 '05a6713584f9e59d4ad07b8fe52ec43dc5cbfe8a09e6cebcef96390262123930'
     end
   end
 
   def install
     if OS.mac?
       bin.install "litra-autotoggle_v#{version}_darwin-universal" => 'litra-autotoggle'
+    elsif Hardware::CPU.arm?
+      bin.install "litra-autotoggle_v#{version}_linux-aarch64" => 'litra-autotoggle'
     else
       bin.install "litra-autotoggle_v#{version}_linux-amd64" => 'litra-autotoggle'
     end
   end
 
   service do
-    run opt_bin/'litra-autotoggle'
+    run opt_bin / 'litra-autotoggle'
     keep_alive crashed: true
   end
 
--- Formula/litra.rb
+++ Formula/litra.rb
@@ -1,24 +1,26 @@
 class Litra < Formula
-  desc "Control your Logitech Litra light from the command line"
-  homepage "https://github.com/timrogers/litra-rs"
-  version "2.0.0"
+  desc 'Control your Logitech Litra light from the command line'
+  homepage 'https://github.com/timrogers/litra-rs'
+  version '2.1.0'
 
-  # Set the URL and SHA256 based on the platform
   if OS.mac?
-    url "https://github.com/timrogers/litra-rs/releases/download/v2.0.0/litra_v2.0.0_darwin-universal"
-    sha256 "51ddcbcf7510ae3945bb2e3addc73c7ef1afb571fbe99f602584641aeaab0abf"
+    url 'https://github.com/timrogers/litra-rs/releases/download/v2.1.0/litra_v2.1.0_darwin-universal'
+    sha256 '8a107bdfe1646b9584a2c26ca4730c64de67df91c03dfc2a490462c8dca1e57f'
   elsif OS.linux?
     if Hardware::CPU.arm?
-      odie "Linux devices with ARM processors are not supported"
+      url 'https://github.com/timrogers/litra-rs/releases/download/v2.1.0/litra_v2.1.0_linux-aarch64'
+      sha256 '0848a3a99ca4e76dd56b26bed472a8d5bd11ce44b92377d9f5c389e8df8f2ac9'
     else
-      url "https://github.com/timrogers/litra-rs/releases/download/v2.0.0/litra_v2.0.0_linux-amd64"
-      sha256 '2d2cd3cdc608f0ff59abe6e36b92ab0610b757590262547dab21795a5316febf'
+      url 'https://github.com/timrogers/litra-rs/releases/download/v2.1.0/litra_v2.1.0_linux-amd64'
+      sha256 '458b9536bac6a151a0a4adf0f7765cec6f0ef8f5a5b6b2c8d1b10ae933d40acc'
     end
   end
 
   def install
     if OS.mac?
       bin.install "litra_v#{version}_darwin-universal" => 'litra'
+    elsif Hardware::CPU.arm?
+      bin.install "litra_v#{version}_linux-aarch64" => 'litra'
     else
       bin.install "litra_v#{version}_linux-amd64" => 'litra'
     end
